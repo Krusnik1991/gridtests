4ca1bb78c99bd8265f5c8379ae59d508edd4f555
Krusnik <krustnik1991@mail.ru>
2019-11-01 19:53:38 +0300

    fst

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+// Sass configuration
+
+// import { task, src, dest, series, watch } from 'gulp';
+// import sass from 'gulp-sass';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(cb) {
+  gulp
+    .src('*.scss')
+    .pipe(sass())
+    .pipe(
+      gulp.dest(function(f) {
+        return f.base;
+      })
+    );
+  cb();
+});
+
+gulp.task(
+  'default',
+  gulp.series('sass', function(cb) {
+    gulp.watch('*.scss', gulp.series('sass'));
+    cb();
+  })
+);
